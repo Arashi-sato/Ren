54fac961ed3778b2ee3f5cbad0b49f4b5488a51d
嵐 佐藤 🌩️ <mavalesunny@gmail.com>
2025-12-22 15:37:16 +0530

    Add Firebase configuration and initialization scripts

---
+++ common/firebase.js
@@ -0,0 +1,19 @@
+<script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyDC9PI7iyNINLkjKjN91L6sZjtpL3qwGc0",
+    authDomain: "sato-arashi.firebaseapp.com",
+    databaseURL: "https://sato-arashi-default-rtdb.firebaseio.com",
+    projectId: "sato-arashi",
+    storageBucket: "sato-arashi.appspot.com",
+    messagingSenderId: "289229920632",
+    appId: "1:289229920632:web:8a7de7d76b7bac1a30108d"
+  };
+
+  const app = firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const db = firebase.database();
+</script>
